6a7cc8ab71b4a3b5924cee739d823815f8349143
Rahul Raj <rrhhll99@gmail.com>
2017-10-07 21:41:15 +0530

    Added firebase library

--- app/src/main/java/com/example/rahulraj/red_revolution/SignUp.java
+++ app/src/main/java/com/example/rahulraj/red_revolution/SignUp.java
@@ -1,13 +1,26 @@
 package com.example.rahulraj.red_revolution;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+    Button generateOtp;
+    EditText phone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        phone = (EditText) findViewById(R.id.editText33);
+        generateOtp = (Button) findViewById(R.id.generate_otp);
+//        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+//                phone,
+//                60,
+//                TimeUnit.SECONDS,
+//                this,
+//        );
     }
 }
