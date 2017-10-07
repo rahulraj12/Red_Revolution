45949ba247cf18433fdcfecd9ea2562746aa9444
Rahul Raj <rrhhll99@gmail.com>
2017-10-07 20:38:08 +0530

    Added login activity, signup activity and string.xml

--- app/src/main/java/com/example/rahulraj/red_revolution/Login.java
+++ app/src/main/java/com/example/rahulraj/red_revolution/Login.java
@@ -1,13 +1,26 @@
 package com.example.rahulraj.red_revolution;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        signup= (Button) findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Login.this,SignUp.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
